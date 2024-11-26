78aea71959295dd0629ca682e02838b0a8549946
AlmeidaFL <ra00303863@pucsp.edu.br>
2024-11-25 23:27:08 -0300

    Changes

---
+++ Prototipo_UCC03/CatalogService/CatalogService/Migrations/20241107111011_SetImageAsOptionalToProduct.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogService.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetImageAsOptionalToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Images_ImageId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ImageId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Products");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductId",
+                table: "Images",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_ProductId",
+                table: "Images",
+                column: "ProductId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Products_ProductId",
+                table: "Images",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Products_ProductId",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_ProductId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Images");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ImageId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ImageId",
+                table: "Products",
+                column: "ImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Images_ImageId",
+                table: "Products",
+                column: "ImageId",
+                principalTable: "Images",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Prototipo_UCC03/CatalogService/CatalogService/Migrations/20241118171528_Removing.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Images");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Images",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
